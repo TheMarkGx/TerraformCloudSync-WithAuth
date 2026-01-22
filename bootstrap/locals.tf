c337c2c85fdcc1741b580c4381f32cd1d3a3cb9d
TheMarkGx <markageiseman@gmail.com>
2026-01-21 23:46:48 -0600

    DynamoDB Compatibility Patch
    Dynamodb doesn't init well with non-deterministic terraform variables like my random_id - converting to deterministic naming per terraform workspace name

--- bootstrap/locals.tf
+++ bootstrap/locals.tf
@@ -2,6 +2,7 @@
 locals {
   Suffix = random_id.suffix.hex
 
-  tfstate_bucket_name = "${var.project_name}-tfstate-${local.Suffix}"
-  lock_table_name     = "${var.project_name}-tflock-${local.Suffix}"
+  tfstate_bucket_name = "${var.project_name}-tfstate-${local.Environment}"
+  lock_table_name     = "${var.project_name}-tflock-"
+  Environment         = terraform.workspace
 }
