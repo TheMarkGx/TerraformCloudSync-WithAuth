2119a2e91fd0b08713b184293e3f49d4d85ebca2
TheMarkGx <markageiseman@gmail.com>
2026-01-23 03:54:28 -0600

    Bugfix
    Corrected dev to default; dynamic multi-env framework not fully implemented to CI/CD yet

--- bootstrap/locals.tf
+++ bootstrap/locals.tf
@@ -3,6 +3,6 @@ locals {
   Suffix = random_id.suffix.hex
 
   tfstate_bucket_name = "${var.project_name}-tfstate-${local.Environment}"
-  lock_table_name     = "${var.project_name}-tflock-"
+  lock_table_name     = "${var.project_name}-tflock-${local.Environment}"
   Environment         = terraform.workspace
 }
