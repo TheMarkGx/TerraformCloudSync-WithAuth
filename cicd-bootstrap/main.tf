71d8706b9356873f1a8dbd07a32b5610e947b0dc
TheMarkGx <markageiseman@gmail.com>
2026-01-22 21:32:18 -0600

    Automation resource ownership
    Added a destroy workflow in support of the new policy that enforces Actions to have full ownership on all CI/CD automated resources

--- cicd-bootstrap/main.tf
+++ cicd-bootstrap/main.tf
@@ -56,8 +56,114 @@ resource "aws_iam_role" "github_actions" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-# Start broad to avoid fighting IAM during early iterations.
-resource "aws_iam_role_policy_attachment" "admin" {
+resource "aws_iam_policy" "terraform_deploy" {
+  name        = "terraform-deploy"
+  description = "Permissions for GitHub Actions to deploy/destroy this Terraform stack."
+  policy      = data.aws_iam_policy_document.terraform_deploy.json
+}
+
+resource "aws_iam_role_policy_attachment" "terraform_deploy" {
   role       = aws_iam_role.github_actions.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  policy_arn = aws_iam_policy.terraform_deploy.arn
+}
+
+data "aws_iam_policy_document" "terraform_deploy" {
+  statement {
+    sid     = "TerraformReadBasics"
+    effect  = "Allow"
+    actions = [
+      "sts:GetCallerIdentity",
+      "ec2:DescribeRegions"
+    ]
+    resources = ["*"]
+  }
+
+  # S3: state + artifacts/buckets managed by this repo
+  statement {
+    sid     = "S3Management"
+    effect  = "Allow"
+    actions = [
+      "s3:CreateBucket",
+      "s3:DeleteBucket",
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+      "s3:GetBucketVersioning",
+      "s3:PutBucketVersioning",
+      "s3:GetBucketPublicAccessBlock",
+      "s3:PutBucketPublicAccessBlock",
+      "s3:GetBucketPolicy",
+      "s3:PutBucketPolicy",
+      "s3:DeleteBucketPolicy",
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:AbortMultipartUpload",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListMultipartUploadParts"
+    ]
+    resources = ["*"]
+  }
+
+  # DynamoDB: terraform lock table
+  statement {
+    sid     = "DynamoDBManagement"
+    effect  = "Allow"
+    actions = [
+      "dynamodb:CreateTable",
+      "dynamodb:DeleteTable",
+      "dynamodb:DescribeTable",
+      "dynamodb:UpdateTable",
+      "dynamodb:TagResource",
+      "dynamodb:UntagResource",
+      "dynamodb:ListTagsOfResource",
+      "dynamodb:PutItem",
+      "dynamodb:GetItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:UpdateItem"
+    ]
+    resources = ["*"]
+  }
+
+  # Lambda + API Gateway + Logs
+  statement {
+    sid     = "LambdaAndAPIGWAndLogs"
+    effect  = "Allow"
+    actions = [
+      "lambda:*",
+      "apigateway:*",
+      "logs:*",
+      "cloudwatch:*"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "IAMForThisStack"
+    effect  = "Allow"
+    actions = [
+      "iam:CreateRole",
+      "iam:DeleteRole",
+      "iam:GetRole",
+      "iam:UpdateRole",
+      "iam:UpdateAssumeRolePolicy",
+      "iam:AttachRolePolicy",
+      "iam:DetachRolePolicy",
+      "iam:PutRolePolicy",
+      "iam:DeleteRolePolicy",
+      "iam:CreatePolicy",
+      "iam:DeletePolicy",
+      "iam:GetPolicy",
+      "iam:GetPolicyVersion",
+      "iam:CreatePolicyVersion",
+      "iam:DeletePolicyVersion",
+      "iam:ListPolicyVersions",
+      "iam:ListAttachedRolePolicies",
+      "iam:ListRolePolicies",
+      "iam:TagRole",
+      "iam:UntagRole",
+      "iam:PassRole"
+    ]
+    resources = ["*"]
+  }
 }
